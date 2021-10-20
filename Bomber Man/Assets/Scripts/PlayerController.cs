2aeab224afcc42832a1f6eb4e98c21b4d1951a9a
JuunasS <62592089+JuunasS@users.noreply.github.com>
2021-10-20 17:20:46 +0300

    Added player movement

--- Bomber Man/Assets/Scripts/PlayerController.cs
+++ Bomber Man/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 5.0f;
+    Rigidbody2D rb2D;
+
+    Vector2 movement;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Transform.Translate();
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb2D.MovePosition(rb2D.position + movement * speed * Time.deltaTime);
     }
 }
