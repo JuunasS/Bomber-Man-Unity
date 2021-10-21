e2eb662bc2569769bedc8839d531df3edf78b92a
JuunasS <62592089+JuunasS@users.noreply.github.com>
2021-10-21 13:27:25 +0300

    Changes to enemy snail

--- Bomber Man/Assets/Scripts/SnailEnemy.cs
+++ Bomber Man/Assets/Scripts/SnailEnemy.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class SnailEnemy : MonoBehaviour
 {
+    public float speed = 2.0f;
+    private Rigidbody2D rb2d;
+
+    public float stopDuration;
+
+    public Transform[] wayPoints;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+
     }
 }
